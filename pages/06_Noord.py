2b06b43ace78dcebfa3e07ac5113391dda9422d8
casijnvantill1 <casijn.van.till@hva.nl>
2024-09-26 15:07:09 +0200

    Update 06_Noord.py

--- pages/06_Noord.py
+++ pages/06_Noord.py
@@ -48,7 +48,7 @@ def create_pie_chart(row, stadsdeel):
     return img_bytes
 
 # Hoofd Streamlit-app
-st.title("Stemverdeling per Stadsdeel in Amsterdam")
+st.title("Stemverdeling van de stemmen in stadsdeel Noord")
 
 # Genereer een taartdiagram voor Noord en toon het
 img_bytes = create_pie_chart(noord_row, noord_row['Stadsdeel'])
