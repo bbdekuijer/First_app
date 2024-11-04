eef519862a85b6194c35033f22378d717a158071
bbdekuijer <bbdekuijer@gmail.com>
2024-11-04 14:15:17 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -11,11 +11,5 @@ st.title("Titanic Data Analyse App")
 # Introductie
 st.write("Welkom bij de Titanic Data Analyse App. Deze app biedt een interactieve manier om de Titanic-dataset te verkennen, visualiseren en te analyseren met behulp van machine learning.")
 
-st.markdown(
-    """
-    <div style="position: fixed; bottom: 10px; left: 10px; padding: 10px;">
-        <img src="Afbeeldingen/Titanic.jpg" alt="Titanic" style="width: 150px; height: auto;">
-    </div>
-    """,
-    unsafe_allow_html=True
-)
+# Afbeelding onderaan de pagina
+st.image("Afbeeldingen/Titanic.jpg", caption="Titanic", use_column_width=True)
