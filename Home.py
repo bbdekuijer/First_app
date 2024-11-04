e2587be85524ea317ff472cecdc2f68a943aabdd
bbdekuijer <bbdekuijer@gmail.com>
2024-11-04 15:23:55 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,30 +1,24 @@
 import streamlit as st
-import pandas as pd
 
-# Laad de dataset
-data_path = "Data/Raw/train.csv"
-data = pd.read_csv(data_path)
-
-# Hoofdtitel met emoji
+# Hoofdtitel
 st.title("Titanic Data Analyse App")
 
-# Introductie met emoji
+# Introductie
 st.write("Welkom bij de Titanic Data Analyse App! Deze app biedt een interactieve manier om de Titanic-dataset te verkennen, visualiseren en te analyseren.")
 
-# Informatie over de dataset
-st.header("Dataset Informatie")
-st.write("Hieronder vind je een samenvatting van de Titanic dataset:")
+# Navigatie
+st.sidebar.title("Navigatie")
+page = st.sidebar.radio("Selecteer een sectie:", ["Data Analyse", "Visualisatie", "Algoritme"])
 
-# Maak twee kolommen voor de data en de statistieken
-col1, col2 = st.columns(2)
+if page == "Data Analyse":
+    st.markdown("Ga naar de pagina voor Data Analyse.")
+    st.write("Hier kun je gegevens verkennen en basisstatistieken bekijken.")
 
-# Eerste kolom voor de eerste paar rijen van de dataset
-with col1:
-    st.subheader("Gegevens")
-    st.dataframe(data.head(8))  # Laat de eerste 8 rijen van de dataset zien
+elif page == "Visualisatie":
+    st.markdown("Ga naar de pagina voor Visualisatie.")
+    st.write("Hier kun je grafieken en visualisaties bekijken.")
 
-# Tweede kolom voor de basisstatistieken
-with col2:
-    st.subheader("Basis Statistieken")
-    st.write(data.describe())  # Basisstatistieken van de dataset
+elif page == "Algoritme":
+    st.markdown("Ga naar de pagina voor Algoritme.")
+    st.write("Hier kun je machine learning modellen toepassen.")
 
