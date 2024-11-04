419d0eb861d4d222c35f90cef20c08ad861d7cda
bbdekuijer <bbdekuijer@gmail.com>
2024-11-04 15:21:49 +0100

    Update 02_Visualisatie.py

--- pages/02_Visualisatie.py
+++ pages/02_Visualisatie.py
@@ -1,11 +1,29 @@
-import plotly.express as px
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 
-st.title("Visualisaties")
+# Laad de dataset
+data_path = "Data/Raw/train.csv"
+data = pd.read_csv(data_path)
 
-# Histogram van de leeftijd
-fig_age = px.histogram(data, x='Age', nbins=30, title='Distributie van Leeftijd')
-st.plotly_chart(fig_age)
+# Hoofdtitel
+st.title("Visualisatie")
 
-# Boxplot van de leeftijd per overleving
-fig_box = px.box(data, x='Survived', y='Age', title='Leeftijd per Overleving')
-st.plotly_chart(fig_box)
+# Introductie
+st.write("In deze sectie kun je grafieken en visualisaties bekijken.")
+
+# Eenvoudig staafdiagram voor overleving
+st.subheader("Aantal Overlevenden")
+survived_counts = data['Survived'].value_counts()
+fig, ax = plt.subplots()
+ax.bar(['Niet Overleefd', 'Overleefd'], survived_counts, color=['red', 'green'])
+ax.set_ylabel('Aantal Passagiers')
+st.pyplot(fig)  # Toon de grafiek in de app
+
+# Histogram van leeftijden
+st.subheader("Leeftijdshistogram")
+fig2, ax2 = plt.subplots()
+ax2.hist(data['Age'].dropna(), bins=30, color='blue', alpha=0.7)
+ax2.set_xlabel('Leeftijd')
+ax2.set_ylabel('Aantal Passagiers')
+st.pyplot(fig2)  # Toon de histogram in de app
