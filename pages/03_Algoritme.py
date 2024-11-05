ae8d0a7f3ce9153c256fca5264bec960a6bbaa9f
bbdekuijer <bbdekuijer@gmail.com>
2024-11-05 23:02:22 +0100

    Update 03_Algoritme.py

--- pages/03_Algoritme.py
+++ pages/03_Algoritme.py
@@ -27,3 +27,6 @@ try:
     """)
 except FileNotFoundError:
     st.error(f"Het bestand `{file_path}` kon niet worden gevonden. Zorg ervoor dat het bestand op de juiste locatie staat.")
+
+# Afbeelding onderaan de pagina
+st.image("Afbeeldingen/Uitkomst.jpg", caption="Titanic", width=200)
