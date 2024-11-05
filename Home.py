e3cae04f4e6621ef364d856e6b8441bab0eae383
bbdekuijer <bbdekuijer@gmail.com>
2024-11-05 22:40:00 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,24 +3,6 @@ import streamlit as st
 # Hoofdtitel
 st.title("Titanic Data Analyse App")
 
-# CSS voor achtergrondafbeelding links onderin
-st.markdown(
-    """
-    <style>
-    .background-image {
-        position: fixed;
-        bottom: 10px;
-        left: 10px;
-        width: 200px;
-        opacity: 0.8;
-        z-index: -1;
-    }
-    </style>
-    <img src='Afbeeldingen/HVA.png' class='background-image'>
-    """,
-    unsafe_allow_html=True
-)
-
 # Introductie
 st.write("Welkom bij de Titanic Data Analyse App! Deze app biedt een interactieve manier om de Titanic-dataset te verkennen, visualiseren en te analyseren.")
 st.write("""
@@ -31,4 +13,7 @@ Met behulp van deze app kun je:
 
 Duik erin en ontdek zelf de patronen en inzichten die de Titanic-data te bieden heeft!
 """)
+
+# Afbeelding onderaan de pagina
+st.image("Afbeeldingen/Titanic.jpg", caption="Titanic", use_column_width=True)
 
