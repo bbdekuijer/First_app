3b34a0b2cb879d9f4e237f26dd24a2470964d66a
bbdekuijer <bbdekuijer@gmail.com>
2024-11-05 22:20:32 +0100

    Update 01_Visualisatie.py

--- pages/01_Visualisatie.py
+++ pages/01_Visualisatie.py
@@ -7,7 +7,20 @@ data_path = "Data/Raw/train.csv"
 data = pd.read_csv(data_path)
 
 # Titel van de visualisatie sectie
-st.title("Interactieve Visualisatie van Overlevingsstatistieken")
+st.title("Visualisatie van gegevens Titanic Dataset")
+
+# Maak twee kolommen voor de data en de statistieken
+col1, col2 = st.columns(2)
+
+# Eerste kolom voor de eerste paar rijen van de dataset
+with col1:
+    st.subheader("Gegevens")
+    st.dataframe(data.head(8))  # Laat de eerste 8 rijen van de dataset zien
+
+# Tweede kolom voor de basisstatistieken
+with col2:
+    st.subheader("Basis Statistieken")
+    st.write(data.describe())  # Basisstatistieken van de dataset
 
 # Selectiebox voor grafiektype
 chart_type = st.selectbox("Selecteer de grafiek die je wilt zien:", 
